558a955f0cd1b403f3d6a370f4a87d8bd7861079
Théo R <31210187+Muskya@users.noreply.github.com>
2021-06-30 18:34:04 +0200

    exo1_cpp_tabChar_developpez fait
    Pour plus d'informations si nécessaire plus tard sur la conversion de l'entier tapé dans la chaîne dans l'entier directement, se référer à la conversion char->int selon la table ASCII.

--- Main.cpp
+++ Main.cpp
@@ -1,62 +1,67 @@
 //Exercice 1 (Tableaux Char)
-/*  */
+/* Écrire une fonction qui a en paramètres une chaîne de caractères (paramètre en entrée)
+et un entier e (paramètre en sortie). Cette fonction renvoie un booléen. La fonction renvoie
+true si la chaîne de caractères est un entier écrit sous la forme d'une suite de chiffres
+qui ne commence pas par 0, elle renvoie false sinon. Si la chaîne est correcte, la fonction
+renvoie dans e la valeur de cet entier. */
 
-//standard library headers
+//SL/STL headers
 #include <iostream> 
 #include <string>
 #include <vector>
 #include <array>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
+bool isStringInteger(char litteral[], int& e) {
+	int i = 0;
+	bool done = false;
 
-	// ----------------- ITERATORS PRACTICE ---------------- //
+	if (litteral[0] == '0')
+		return done;
 
-	std::array<char, 6> const mot{ 'C','H','E','V','A','L' }; //some const char array
-	std::array<char, 6>::const_iterator debut{ mot.begin() }; //beginning iterator (needs to be constant
-	//because it operates on a const container)
-	auto fin{ mot.end() }; //ending iterator (used auto here)
+	while (litteral[i] != '\0') {
+		if ((litteral[i] >= '1' && litteral[i] <= '9')
+			|| (litteral[i] == '0' && i > 0)) //0 seulement si ce n'est pas le premier element
+		{
+			done = true;
+			e = 10 * e + (litteral[i] - '0'); //'0' = 48 is ascii table. refer to it for further
+											  //explanations.
+		} 
+		else {
+			done = false;
+		}
+		i++;
+	}
 
-	std::vector<int> chiffres{ 10, 20, 100, 500, 10000, 55555 }; //some int vector
-	std::vector<int>::iterator iterChiffresDebut{ chiffres.begin() }; //beginning iterator (not used here)
-	std::vector<int>::iterator iterChiffresEnd{ chiffres.end() - 1 }; //ending iterator.
-	//the ending iterator points to the container.lastElement() + 1 which means a null-value place so that we're sure
-	//we've reached the end of the container/collection. when using its value we must then proceed to decrement
-	//it at least once otherwise the compiler will be ANGRY af.
-
-	//ITERATORS ARE POINTERS so we need to dereference them with * to use them properly.
-
-	//Iterates through "array<char, 6> mot" with a precedently declared iterator
-	cout << "Affichage de \"array<char,6> mot\" with for avec iterateurs debut et fin" << endl; 
-	for (debut; debut < fin; debut++)
-		cout << *debut;
-	cout << "\n\n";
-
-	//Iterates through "vector<int> chiffres" with a local iterator created in the loop
-	cout << "Affichage de \"vector<int> chiffres\" with for avec iterateur cree localement" << endl;
-	for (std::vector<int>::iterator it{ chiffres.begin() }; it != chiffres.end(); it++)
-		cout << *it << endl;
-	cout << "\n";
-
-	//Iterates reversely (rbegin = reverse begin, rend = reverse end)
-	cout << "Affichage de \"vector<int> chiffres\" a l'envers avec rbegin()/rend()" << endl;
-	for (auto it{ chiffres.rbegin() }; it != chiffres.rend(); ++it)
-		std::cout << *it << std::endl;
-	cout << "\n";
-
-	--iterChiffresEnd; //could also initialize the iterator to "chiffres.end() - 1" earlier.
-	cout << "Affichage dernier element \"vector<int> chiffres\" en decrementant iterator initialise a end()" << endl;
-	cout << *iterChiffresEnd << endl; //should output the last element of vector<int> chiffres (55555).
-	cout << "\n";
-
-	cout << "Creation d'un container sous-ensemble de \"vector <int> chiffres\" avec les elements 3/4/5 (indices 2/3/4)" << endl;
-	std::vector<int> sousChiffres{ chiffres.begin() + 2, chiffres.begin() + 5 };
-	for (int element : sousChiffres)
-		cout << element << ", ";
-	cout << "\n";
+	return done;
 }
 
+int main() {
+	//TEST RECONNAISSANCE NULL-CHARACTER//
+	char litteral[] = "Hello";
+	cout << litteral[4] << endl;
+	if (litteral[5] == '\0') {
+		cout << "dernier element == \\0" << endl;
+	}
+	cout << "\n\n";
+
+	//-- TEST FONCTION EXERCICE --//
+	char inp[30];		//30 chars is enough for testing
+	int e = 0;			//entier passé en référence qui prendra la valeur 
+						//de l'entier écrit dans la chaîne
+	do {
+		cout << "Entrez une phrase (max 30carac.): " << endl;
+		cin >> inp;
+		if (isStringInteger(inp, e)) {
+			cout << "Votre phrase comprend bien une sequence d'entiers." << endl;
+			cout << "Voici la chaine tapee convertie en entier: " << e << endl;
+		}
+		else
+			cout << "Pas de sequence d'entiers detectee." << endl;
+	} while (!isStringInteger(inp, e));
+}
 
 
 
