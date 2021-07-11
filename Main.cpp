1abdf9bb7ca11f39e2aab404da9f71ac5faded9a
Théo R <31210187+Muskya@users.noreply.github.com>
2021-07-11 21:48:54 +0200

    little macros/cond. ope/constexpr practice

--- Main.cpp
+++ Main.cpp
@@ -1,38 +1,40 @@
+#include <iostream>
 
-#pragma region Includes
-/*SL headers*/
-	//basics
-	#include <iostream> 
-	#include <string>
-	#include <utility>
-	#include <cstdlib> //for rand()
-	#include <fstream>
-/*STL headers*/
-	#include <algorithm>
-		//containers
-			//sequencials
-			#include <vector> 
-			#include <deque>
-			#include <array>
-			#include <stack>
-			#include <queue> //contains priority-queue as well
-			#include <list>
-			//associatives
-			#include <map> //includes multimap.h
-			#include <set> //includes multiset.h
-		//pre-defined STL functors
-		//https://www.cplusplus.com/reference/functional/
-		#include <functional>
-		//iterators
-		#include <iterator>
-#pragma endregion
-
-using namespace std;
+// These are "object-like" macros. They can have substitution values or not.
+#define HELLO_FR "Bonjour"
+#define PI_VALUE 3.14
+#define MY_AGE 22
+#define YES true
 
 int main() {
-	
-}
+	// Macros testing
+	std::cout << HELLO_FR << std::endl; // Will print "Bonjour" to the console
+	std::cout << "Pi value is equal to " << PI_VALUE << std::endl;
+	std::cout << "I was worn on the 22th of July 1999. My age is " << MY_AGE << std::endl;
 
+	bool is_cpp_nice = YES;
+	is_cpp_nice == YES ? std::cout << "Cpp is nice." : std::cout << "Cpp is not nice.";
+
+	/* Conditional operator testing
+	Only use the conditional operator for simple conditionals 
+	where you use the result and where it enhances readability. */
+	bool higherThan = false;
+	short a = 0, b = 0;
+	std::cout << "Input a and then b :" << std::endl;
+	std::cin >> a; std::cin >> b;
+	a > b ? higherThan = true : higherThan = false;
+	if (higherThan)
+		std::cout << "a is higher than b" << std::endl;
+	else
+		std::cout << "a is equal or lower than b" << std::endl;
+
+	/* Const values are known/resolved at compile-time, so we should be
+	* declaring them as "constexpr" for more specificity. (C++11)
+	Const values that are resolved at runtime only should be declared
+	as const only. */
+	constexpr float gravity = 9.8;
+	constexpr short sum = 10 + 5; 
+}
 
 
 
