bc7872b9f5a05eaf066f536fb9c67d4fb8e2f95e
Théo R <31210187+Muskya@users.noreply.github.com>
2021-08-06 16:02:18 +0200

    more thread practice (1)

--- Main.cpp
+++ Main.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 
 // Some threading-test functions
-void to20() {
-	for (int i = 0; i < 20; i++) {
-		std::cout << "Boucle une: " << i << std::endl;
-	}
-}
-void to10() {
-	for (int i = 0; i < 10; i++) {
-		std::cout << "Boucle deux: " << i << std::endl;
-	}
+void testOne(int a, int b) {
+	bool higherThan = false;
+	a > b ? higherThan = true : higherThan = false;
+	if (higherThan)
+		std::cout << a << " is superior to " << b << std::endl;
+	else
+		std::cout << a << " is not superior to " << b << std::endl;
 }
 void saySmth(std::string str) {
 	std::cout << str << std::endl;
 }
 
+void to10A() {
+	for (int i = 0; i < 10; i++) {
+		std::cout << "A: " << i << std::endl;
+	}
+}
+void to10B() {
+	for (int i = 0; i < 10; i++) {
+		std::cout << "B: " << i << std::endl;
+	}
+}
+
 int main() {
+
 	/* C++ THREADS PRACTICE */
 
-	// Create some thread objects
-	std::thread t1(to20);
-	std::thread t2(to10);
+	int a, b;
+	std::cout << "Input value a then value b: " << std::endl;
+	std::cin >> a; std::cin >> b; std::cout << "\n";
 
-	/* You can specify the attached function's arguments 
-    in the constructor of the thread. */
-	std::thread t3(saySmth, "Bonjour tout le monde !");
+	//	Fetches current thread's ID (it will be Main thread here)
+	std::thread::id main_thread_id = std::this_thread::get_id();
+	std::cout << "Main thread ID: " << main_thread_id << std::endl;
 
-	// Make sure threads have ended before ending the program
-	t1.join(); t2.join(); t3.join();
+	//	Create some thread objects
+	std::thread t1(testOne, a, b);
+
+	/*	You can specify the attached function's arguments 
+		in the constructor of the thread. */
+	std::thread t2(saySmth, "Bonjour tout le monde !");
+
+	//	Fetches ongoing threads' IDs
+	std::cout << "t1 ID: " << t1.get_id() << std::endl;
+	std::cout << "t2 ID: " << t2.get_id() << std::endl;
+
+	//	Make sure threads have ended before ending the program
+	t1.join(); t2.join();
+	std::cout << "\n\n" << std::endl;
+
 }
 
 
