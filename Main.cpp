b49355a5414533f5d1e68841d81189bc455f43a9
Théo R <31210187+Muskya@users.noreply.github.com>
2021-08-07 22:17:37 +0200

    started CW Kata: Descending Order C++
    (not finished yet)
    https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <cinttypes>
+#include <cstddef>
+
+/* https://CODEWARS.COM/KATA/5467e4d82edf8bbf40000155/train/cpp */
+/* Your task is to make a function that can take any non-negative 
+integer as an argument and return it with its digits in descending order.
+Essentially, rearrange the digits to create the highest possible number.*/
+
+typedef unsigned short int short_int;
+
+uint64_t descendingOrder(uint64_t a) // a = 15 in this example
+{
+	short_int reversed = 0;
+	short_int n = 0, nbDigits = 0;
+	while (a != 0) {
+		a /= 10; // 1: 1.5 ||| 2: 0.15
+		nbDigits++;
+	}
+}
 
 int main() {
+	uint64_t a;
+	std::cout << "Please input a number: " << std::endl;
+	std::cin >> a;
 
+	descendingOrder(a);
+
+	return 0;
 }
 
 
