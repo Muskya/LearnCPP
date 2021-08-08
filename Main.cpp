6766163db1396b96b71925f3108fc1693fd060bb
Théo R <31210187+Muskya@users.noreply.github.com>
2021-08-08 22:30:38 +0200

    CW Kata Descending Order Solution
    Not by me. This is so clever.

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cinttypes>
-#include <cstddef>
-#include <vector>
+#include <string>
 #include <algorithm>
 
 /* https://CODEWARS.COM/KATA/5467e4d82edf8bbf40000155/train/cpp */
@@ -9,39 +8,13 @@
 integer as an argument and return it with its digits in descending order.
 Essentially, rearrange the digits to create the highest possible number.*/
 
-typedef unsigned short int short_int;
 
-void descendingOrder(uint64_t a) // a = 1021 in this example
+// // // // SOLUTION // // // //
+uint64_t descendingOrder(uint64_t a)
 {
-	short_int
-		remainder	= 0,		//	Remainder (last digit of the number at each iteration)
-		maxDigit	= 0,		//	Digit that will go first
-		descending	= 0;		//  Final descending number
-
-	std::vector<short_int> rest;
-
-	while (a != 0) {
-		remainder = a % 10; 
-		std::cout << "Remainder: " << remainder << std::endl; 
-		if (remainder > maxDigit) {
-			if (maxDigit == 0) {}
-			else { rest.push_back(maxDigit); }
-			maxDigit = remainder; 
-			descending = descending * 10 + maxDigit;
-		} else {
-			rest.push_back(remainder);
-		}
-		a /= 10;
-	}
-
-	while (rest.size() > 1) {
-		descending = (descending * 10) + *max_element(rest.begin(), rest.end());
-		rest.erase(max_element(rest.begin(), rest.end()));
-	}
-	descending = descending * 10 + rest.front(); // Final addition
-
-	std::cout << "Number input in descending order:" << std::endl;
-	std::cout << descending << std::endl;
+	std::string s = std::to_string(a);
+	std::sort(s.rbegin(), s.rend());
+	return std::stoull(s);
 }
 
 int main() {
@@ -49,7 +22,7 @@ int main() {
 	std::cout << "Please input a number: " << std::endl;
 	std::cin >> a;
 
-	descendingOrder(a);
+	std::cout << "Number input in descending order: " << descendingOrder(a) << std::endl;
 
 	return 0;
 }
