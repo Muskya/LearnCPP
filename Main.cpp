eaee557a24f54a795e2f050ce54cc724bc6213ac
Théo R <31210187+Muskya@users.noreply.github.com>
2021-08-11 11:01:44 +0200

    removed some obsolete files

--- Main.cpp
+++ Main.cpp
@@ -1,30 +1,12 @@
 #include <iostream>
-#include <cinttypes>
-#include <string>
-#include <algorithm>
 
-/* https://CODEWARS.COM/KATA/5467e4d82edf8bbf40000155/train/cpp */
-/* Your task is to make a function that can take any non-negative 
-integer as an argument and return it with its digits in descending order.
-Essentially, rearrange the digits to create the highest possible number.*/
-
-
-// // // // SOLUTION // // // //
-uint64_t descendingOrder(uint64_t a)
-{
-	std::string s = std::to_string(a);
-	std::sort(s.rbegin(), s.rend());
-	return std::stoull(s);
-}
+/* https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/cpp */
+/* Create a function that returns the sum of the two lowest positive
+numbers given an array of minimum 4 positive integers. No floats or 
+non-positive integers will be passed.*/
 
 int main() {
-	uint64_t a;
-	std::cout << "Please input a number: " << std::endl;
-	std::cin >> a;
 
-	std::cout << "Number input in descending order: " << descendingOrder(a) << std::endl;
-
-	return 0;
 }
 
 
