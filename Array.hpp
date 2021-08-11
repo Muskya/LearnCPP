176c6a2e7b693672fd8ff7c221a90bef13160aff
Théo R <31210187+Muskya@users.noreply.github.com>
2021-08-11 19:04:56 +0200

    some more template practice (1)
    there will be a last part which is number 2

---
+++ Array.hpp
@@ -0,0 +1,60 @@
+// This is the templated version of the IntArray we made before.
+// We will now be able to handle all the fundamental data types
+// in just one class.
+
+// This is named ".hpp". Sort of a personal convention because
+// templates definition are commonly placed into their 
+// standard .h declaration file.
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <cassert> // For Assert()
+
+template <class T>
+class Array
+{
+private:
+    int m_length{};
+    T* m_data{};
+public:
+    Array(int length)
+    {
+        assert(length > 0);
+        m_data = new T[length]{};
+        m_length = length;
+    }
+
+    Array(const Array&) = delete;
+    Array& operator=(const Array&) = delete;
+
+    ~Array()
+    {
+        delete[] m_data;
+    }
+
+    void erase()
+    {
+        delete[] m_data;
+        m_data = nullptr;
+        m_length = 0;
+    }
+
+    T& operator[](int index)
+    {
+        assert(index >= 0 && index < m_length && "Index out of bounds");
+        return m_data[index];
+    }
+
+    // templated getLength() function defined below
+    int getLength() const;
+};
+
+// Function definitions //
+
+template<typename T>
+int Array<T>::getLength() const {
+    return m_length;
+}
+
+#endif
--- Main.cpp
+++ Main.cpp
@@ -2,16 +2,14 @@
 #include <iostream>
 
 // User-Defined Headers
-#include "IntArray.h"
+#include "Array.hpp"
+#include "SomeTemplateTests.hpp"
 
 // This is a FUNCTION template.
 template<typename T>
 T Max(T a, T b) {
 	return a < b ? a : b;
 }
-
-// This is a CLASS template.
-// ...
 
 int main() {
 	/* TEMPLATE PRACTICE */
@@ -21,9 +19,11 @@ int main() {
 	std::cout << Max(a, b) << std::endl;
 	std::cout << Max(c, d) << std::endl;
 
-	std::cout << "\n\n";
+	std::cout << "\n\n";	
 
-	
+	SomeTemplateTests::swap<int>(23, 49);
+	std::cout << "\n";
+	SomeTemplateTests::multiples<int, double>(28, 5.3, 5);
 }
 
 
