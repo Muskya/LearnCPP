c83d9fb3d4bfda93a068d07aadc769bf0f7cffb4
Théo R <31210187+Muskya@users.noreply.github.com>
2021-08-15 01:02:30 +0200

    size func, ope[] for other containers, small adds

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <array>
 
 #include "MultiContainer.hpp"
 
 int main() {
-	
+	MultiContainer<int>* mc1 = new MultiContainer<int>(ARR, 10);
+	MultiContainer<int>* mc2 = new MultiContainer<int>(VEC);
+
+	for (int i = 0; i < 10; i++) {
+		(*mc2)[i] = i + i;
+	}
+
+	std::cout << (*mc2)[2] << std::endl;
+	//std::cout << mc2->size() << std::endl;
 }
--- MultiContainer.hpp
+++ MultiContainer.hpp
@@ -24,24 +24,32 @@ template <class T>
 class MultiContainer
 {
 private:
+	// FIELDS
 	T* _data;
 	int _length;
 	Storage _storage;
 public:
+	// CONSTRUCTORS DESTRUCTORS
 	~MultiContainer() = default;
 	MultiContainer() = default;
-	MultiContainer(Storage type, int length);
+	MultiContainer(Storage type, int length = 0);
 
 	// DATA MANIPULATION
 	bool add_back(T val);
 	bool remove_back();
+	// begin(), end() [allocators?]
 
-	// DATA 
-	int length() const; // Used for Arrays
+	// DATA DATA
+	int length() const; // Arrays Exclusively
 	int size() const; 
+
+	// OPERATORS
+	T& operator[](int index);
+	T& operator=(T& const mc) = delete; // No instance copying (for now)
 };
 
 // Main MultiContainer constructor
+// Don't add a length in the case of anything but an array
 template <class T>
 MultiContainer<T>::MultiContainer(Storage type, int length)
 	: _storage(type), _length(length)
@@ -69,13 +77,60 @@ template <class T>
 int MultiContainer<T>::length() const {
 	// Make sure it's an array
 	// Length() method makes no sense for other containers
-	assert(_storage == ARR); 
+	assert(_storage == ARR && "Container is not an array."); 
 	return _length;
 }
 
 // Returns the size (number of elements) of the container
 template <class T>
 int MultiContainer<T>::size() const {
+	int size = 0;
+	T* elemPtr = nullptr;
+	if (_storage == ARR) {
+		if (this->length() == 0)
+			return 0;
+		assert(this->length() > 0);
+		// Logic is : if our pointer elemPtr who's pointing to the
+		// &_data[i] element is nullptr, it means there are no more elements
+		// in the array
+		for (int i = 0; i < this->length(); i++) {
+			elemPtr = &_data[i];
+			if (elemPtr == nullptr) {}
+			else { size++; }
+		}
+	}
+	else { // Other containers have dynamic length
+		int n = 0;
+		if (&_data[n] == nullptr) // If there are no elements yet
+			return 0;
+		else {
+			while (n < INT_MAX) { // I feel like this is bad practice kek
+				n++;
+				if (&_data[n] != nullptr) {
+					elemPtr = &_data[n];
+					size++;
+				}
+				else { break; }
+			}
+		}
+	}
+	return size;
+}
 
+template <class T>
+T& MultiContainer<T>::operator[](int index) {
+	// If we are operating on an array and thus with
+	// fixed-length
+	if (_storage == ARR) {	
+		assert(index >= 0 && index < _length && "Index out of bounds.");
+		return _data[index];
+	}
+	// If we are operating on any other container type
+	else {
+		// Simply increment container length and return
+		// the data at the specified index
+		_length++;
+		return _data[index];
+	}
 }
 #endif
