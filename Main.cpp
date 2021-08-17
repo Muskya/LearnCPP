05385c3eaf95e3ad76c33f1fc1e73b2cca012402
Théo R <31210187+Muskya@users.noreply.github.com>
2021-08-17 21:42:05 +0200

    rmved multicontainer, road map update
    multicontainer was such a shit idea
    even if its not, i dont have the required knowledge about containers and C++ concepts to re-implement this sort of thing
    main goal is to practice the containers now

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,18 @@
+// Basic
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <array>
 
-#include "MultiContainer.hpp"
+// STL Containers
+#include <vector>
+#include <array>
+#include <list>
+#include <forward_list>
+#include <stack>
+//#include <tree?>
+#include <map>
+#include <hash_map>
+#include <set>
+#include <algorithm>
 
 int main() {
-	MultiContainer<int>* mc1 = new MultiContainer<int>(ARR, 10);
-	MultiContainer<int>* mc2 = new MultiContainer<int>(VEC);
-
-	mc2->add_back(25);
-	mc2->add_back(45);
-	mc2->add_back(65);
-
-	for (int i = 0; i < mc2->size(); i++) {
-		std::cout << (*mc2)[i] << std::endl;
-	}
 	
-
-	//std::cout << mc2->size() << std::endl;
 }
