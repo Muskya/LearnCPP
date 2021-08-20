84cf0ef1490e84bb2572e0c74435b47c5ee07ccb
Théo R <31210187+Muskya@users.noreply.github.com>
2021-08-20 17:46:41 +0200

    core working

--- Array.hpp
+++ Array.hpp
@@ -1,6 +1,8 @@
 // std::array Implementation by Théo R.
 // 18th August 2021
 // Array.hpp
+// This is just practice. I won't be implementing all the 
+// constant versions of member functions. (or later kek)
 /* references : 
 *
 * https://devdocs.io/cpp/container/array
@@ -8,13 +10,18 @@
 * https://en.cppreference.com/w/cpp/container/array
 */
 
-// Details learnt about some of the concepts introduced in this .hpp :
+// Details learnt while writing this implementation :
 /*  constexpr:      value/return value is constant and evaluated at compile-time.
 *                   can be used whenever const value is needed, in templates 
 *                   or array declarations for example.
 *   noexcept:       compile-time check operator that returns true if an member func
 *                   has a "noexcept" specifier in its declaration. For the member,
 *                   noexcept means it will throw no exception.
+*   POD:            "Plain Old Data". Class/Struct without any (virtual)functions,
+*                   constructor, or destructor. 
+*   Aggregate:      An aggregate is an array or a class with no user-declared 
+                    constructors, no private or protected non-static data members,
+                    no base classes, and no virtual functions.
 */
 
 #ifndef ARRAY_HPP
@@ -50,12 +57,15 @@ public:
     Array(const Array&) = delete; // To change
     Array& operator=(const Array&) = delete; // To change
 
-    // ELEMENT ACCESS
-    constexpr _Ty* data();
+    // ELEMENT ACCESS/MANIPULATION
+    _Ty* data() noexcept;
+    const _Ty* data() const noexcept;
+    void fill(_Ty value, int beg = 0, int end = _Size-1);
+    constexpr void swap(Array<_Ty, _Size>& other) noexcept;
+    constexpr _Ty front() const noexcept;
+    constexpr _Ty back() const noexcept;
+    constexpr _Ty at(_STD size_t index) const noexcept;
     constexpr _Ty& operator[](size_type index);
-    constexpr void fill();
-    constexpr void swap();
-    //front, back
 
     // CAPACITY
     constexpr std::size_t size() const noexcept;
@@ -66,6 +76,17 @@ public:
     constexpr iterator begin() noexcept;
     constexpr iterator end() noexcept;
     // cbegin, cend, rbegin, rend, crbegin, crend..........
+
+    // MISC
+    void show() {
+        std::cout << "\n";
+        for (int i = 0; i < _Size; i++) {
+            if (i == _Size - 1)
+                std::cout << m_data[i];
+            else
+                std::cout << m_data[i] << ", ";
+        }
+    }
 };
 
 // some writing shortcuts
@@ -73,16 +94,59 @@ public:
 #define ARRAY Array<_Ty, _Size> 
 
 /*----ELEMENT ACCESS----*/
-// returns underlying C-Array
+// returns underlying c-array
 TEMPLATE
-constexpr _Ty* ARRAY::data() {
+_Ty* ARRAY::data() noexcept {
     return m_data;
 }
 
-// []
+// returns underlying const c-array
+TEMPLATE
+const _Ty* ARRAY::data() const noexcept {
+    return m_data;
+}
+// fills the array with the specified value. whole array
+// is filled if indexs are not specified.
+TEMPLATE
+void ARRAY::fill(_Ty value, int beg, int end) {
+    for (int i = beg; i <= end; i++) {
+        m_data[i] = value;
+    }
+}
+// swaps the arrays elements
+TEMPLATE
+constexpr void ARRAY::swap(Array<_Ty, _Size>& other) noexcept {
+    assert(size() == other.size() && "arrays are not the same size");
+    Array<_Ty, _Size>* tmp = new Array<_Ty, _Size>();
+    tmp->fill(0);
+    for (int i = 0; i < _Size; i++) {
+        (*tmp)[i] = m_data[i];
+        m_data[i] = other[i];
+        other[i] = (*tmp)[i];
+    }
+    delete tmp;
+}
+// returns the first item of the array
+TEMPLATE
+constexpr _Ty ARRAY::front() const noexcept {
+    return *begin();
+}
+// returns the last item of the array
+TEMPLATE
+constexpr _Ty ARRAY::back() const noexcept {
+    return *end();
+}
+// returns the item at the specified index
+// features bounds checking.
+TEMPLATE
+constexpr _Ty ARRAY::at(_STD size_t index) const noexcept {
+    assert(index >= 0 && index < _Size && "index ouf of bounds");
+    return m_data[index];
+}
+
+// [] overload. for bounds checking, use at()
 TEMPLATE
 constexpr _Ty& ARRAY::operator[](_STD size_t index) {
-    assert(index >= 0 && index < _Size && "index out of bounds");
     return m_data[index];
 }
 
@@ -103,15 +167,16 @@ constexpr bool ARRAY::empty() const noexcept
 { return (this->size() == 0); }
 
 /*----ITERATORS----*/
-// iterator at the beginning of the array
+// iterator pointing at the last array's element address
 TEMPLATE
 constexpr _Ty* ARRAY::begin() noexcept {
     return &m_data[0];
 }
 
+// iterator pointing at the first array's element address
 TEMPLATE
 constexpr _Ty* ARRAY::end() noexcept {
-    return &m_data[_Size - 1];
+    return &m_data[_Size];
 }
 
 #endif // _STL_COMPILER_PREPROCESSOR
--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,21 @@
 // Basic
 #include <iostream>
-
 // C/C++ Standard Library Headers
 #include <iostream>
 #include <string>
 // STL Headers
 #include <array>
 #include <vector>
+#include <algorithm>
 // User-Defined Headers
 #include "Array.hpp"
 
 int main() {
-	//Array<int, 2>* arr1 = new Array<int, 2>{ 1,2 };
-	Array<int, 4> arr2{ 10, 20, 30, 40 };
-	Array<int, 10> arr3{ 1, 2, 3, 4, 5,
-							6, 7, 8, 9, 10 };
+	Array<int, 3> arr1{ 1, 2, 3 };
+	Array<int, 3> arr2{ 10, 20, 30 };
+	arr1.show(); arr2.show();
 
-	
-	for (int i = 0; i < arr3.size(); i++) {
-		std::cout << arr3[i] << ", ";
-	}
+	std::cout << "\nSwap!" << std::endl;
+	arr1.swap(arr2);
+	arr1.show(); arr2.show();
 }
