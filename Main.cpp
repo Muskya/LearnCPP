0e5b7ecdd4d4d23f389f41f18381278ad3b6dc5a
Théo R <31210187+Muskya@users.noreply.github.com>
2021-09-27 22:00:52 +0200

    some std::move trying

--- Main.cpp
+++ Main.cpp
@@ -8,91 +8,48 @@
 #include <list>
 #include <algorithm>
 #include <memory>
+#include <utility>
 // User-Defined Headers
 #include "Array.hpp"
 #include "Vector.hpp"
 #include "List.hpp"
 
 /* STARTING CHAPTER M FROM LEARNCPP.COM 
-https://www.learncpp.com/cpp-tutorial/intro-to-smart-pointers-move-semantics/
+* https://www.learncpp.com/cpp-tutorial/intro-to-smart-pointers-move-semantics/
+* Another very great reference for rvalue refs : 
+* https://www.cprogramming.com/c++11/rvalue-references-and-move-semantics-in-c++11.html
 */
 
-class Object; // Forward declaration for template parameter just below
+// some r-value functions
+int getTen() {
+    return 10;
+}
+int getTwenty() {
+    return 20;
+}
 
-// class that will hold a pointer. makes it a "smart pointer"
-// because when the local objects of type Box will go out of scope, 
-// constructor will be called and the pointer it contains will be deleted.
-// we don't have to manually delete it and avoids forgetting to do it and other issues.
-template <class T = Object> // Object class by default
-class Box {
-private:
-    T* objInBox;
-public:
-    Box(T* obj = nullptr) : objInBox(obj) {}
-    ~Box() { delete objInBox; } // Pointer's self-cleanup after going out of scope
-
-    /* COPY SEMANTICS */
-    // Copy constructor
-    Box(const Box& b) = delete; // when a user-defined type implements move semantics,
-                                // the copy semantics operations (assigment and constructor)
-                                // are usually deleted.
-
-    // Copy assignment operator
-    Box& operator=(const Box& b) = delete; // same explanation as above
-
-    /* MOVE SEMANTICS */
-    // Move constructor
-    Box(Box&& b) {               // non-const lvalue reference
-        objInBox = b.objInBox;  // transfer other object into this box
-        b.objInBox = nullptr;   // make sure other object is not owned anymore
-    }
-
-    // Move assignment operator
-    Box& operator=(Box&& b) noexcept {    // non-const lvalue reference
-        if (&b == this)
-            return *this;
-
-        // Release any resource we're holding first
-        delete objInBox;
-        
-        // Move the resource (transfer ownership)
-        objInBox = b.objInBox;
-        b.objInBox = nullptr;   // set object's source ownership to
-                                // null, but don't delete allocated space
-
-        return *this;
-    }
-
-    // Some operators overloading
-    T& operator*() const { return *objInBox; }
-    T* operator->() const { return objInBox; }
-
-    // Simple function to check pointer's ownership 
-    // during operations with move semantics
-    void isNull() const {
-        if (objInBox == nullptr)
-            std::cout << "The object in this box is null" << std::endl;
-        else
-            std::cout << "The object in this box is not null" << std::endl;
-    }
-};  
-
-class Object {
-public:
-    Object() { std::cout << "Object put in box." << std::endl; }
-    ~Object() { std::cout << "Object in box deleted. (out of scope)" << std::endl; }
-};
-
-Box<Object> getObject() {
-    Box<Object> obj(new Object());
-    // this will invoke move constructor. obj is a l-value, automatically
-    // returned by the function. it calls move constructor because of 
-    // the c++ specification.
-    return obj; 
+// use std::move(x) to cast lvalue x into a r-value reference
+template <typename T>
+void swap(T& a, T& b) {
+    T temp = std::move(a);
+    a = std::move(b);
+    b = std::move(temp);
 }
 
 int main()
 {
-    Box<Object> mainObj;
-    mainObj = getObject(); // this will ivoke move assignment operator
+    int a = 10;
+    int b = 20;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+    std::cout << "swap" << std::endl;
+    swap(a, b);
+    // swap(getTen(), getTwenty()) doesn't work
+    // swap parameters are l-value references, nothing else
+    // parameters should be const T& a / const T& b to accept any parameter type
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
 }             
