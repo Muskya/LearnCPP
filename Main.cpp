d1243db650872c7b9eac96c008d6226b37c5c234
Théo R <31210187+Muskya@users.noreply.github.com>
2021-10-31 15:36:43 +0100

    tip to calculate code time

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 // C/C++ Standard Library Headers
 #include <iostream>
 #include <string>
+#include <time.h>
 #include <typeinfo> 
 // STL Headers
 #include <array>
@@ -9,23 +10,23 @@
 #include <algorithm>
 #include <memory>
 #include <utility>
-// User-Defined Headers
-#include "Array.hpp"
-#include "Vector.hpp"
-#include "List.hpp"
 
-void digitize(unsigned long n)
+void copyVector(unsigned long n)
 {
-	std::string str = std::to_string(n);
-	std::reverse(str.begin(), str.end());
-	std::vector<int> rvsd;
-	for (int i = 0; i < str.length(); ++i)
-		rvsd.push_back(str[i] - '0');
-	for (int i = 0; i < rvsd.size(); ++i)
-		std::cout << rvsd[i] << std::endl;
+	std::vector<int> myVector;
+	for (int i = 0; i < n; ++i) {
+		myVector.push_back(n / 10 + i);
+	}
+	std::vector<int> vec2;
+	for (int i = myVector.size()-1; i >= 0; --i) {
+		vec2.push_back(myVector[i]);
+	}
 }
 
 int main()
 {
-	digitize(123456);
+	clock_t start = clock();
+	copyVector(12345);
+	clock_t end = clock();
+	std::cout << "Took " << (float)(end - start) / CLOCKS_PER_SEC << " seconds." << std::endl;
 }
