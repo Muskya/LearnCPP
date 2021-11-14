7ceab0fe8401025e23d5539426115f6379511d61
Théo R <31210187+Muskya@users.noreply.github.com>
2021-11-14 20:56:09 +0100

    fetching client and lib apps thread/process id

--- Main.cpp
+++ Main.cpp
@@ -18,20 +18,7 @@
 int main()
 {
 	// DLL STUFF
-	char inp;
-	DWORD a = 50, b = 100;
-	TheoDLL::Action _currentAction;
-
-	do {
-		std::cout << "Assign an action ? (y) hehe" << std::endl;
-		std::cin >> inp;
-	} while (inp != 'y');
-	_currentAction = TheoDLL::Action::Working;
-	// operator<< overloaded (in DLL) for Action type in 
-	std::cout << "Action: " << _currentAction << std::endl;
-
-	TheoDLL::sayHi("Bonjour hello hola");
-	TheoDLL::swap(a, b);
-	std::cout << "a=" << a << std::endl;
-	std::cout << "b=" << b << std::endl;
+	std::cout << "(LearnCPP) Thread ID: " << GetCurrentThreadId() << std::endl;
+	std::cout << "(LearnCPP) Process ID: " << GetCurrentProcessId() << std::endl;
+	TheoDLL::doWindowsAPIStuff();
 }
