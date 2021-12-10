d3e96c8afcfbd803e846516fd0308b4e26f47db1
Théo R <31210187+Muskya@users.noreply.github.com>
2021-12-10 10:19:52 +0100

    Kata 5556282156230d0e5e000089

--- Main.cpp
+++ Main.cpp
@@ -15,10 +15,15 @@
 // User-Defined .DLLs/.LIBs
 #include "Status.h"
 
-int main()
-{
-	// DLL STUFF
-	std::cout << "(LearnCPP) Thread ID: " << GetCurrentThreadId() << std::endl;
-	std::cout << "(LearnCPP) Process ID: " << GetCurrentProcessId() << std::endl;
-	TheoDLL::doWindowsAPIStuff();
+// https://www.codewars.com/kata/5556282156230d0e5e000089/train/cpp 10/Dec/2021
+std::string dnaToRna(std::string dna) {
+	for (size_t i = 0; i < dna.length(); ++i) {
+		if (dna[i] == 'T')
+			dna[i] = 'U';
+	}
+	return dna;
+}
+
+int main() {
+	std::cout << dnaToRna("GCAT ACGT GTAT GACT GATT GACUT AAGT AGCT") << std::endl;
 }
